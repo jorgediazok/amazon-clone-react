a622639b5947c4c192a4ae5c1cd7d21b257b254c
Jorge <jorgediazok@gmail.com>
2020-10-10 11:04:23 -0300

    working on navbar. Added logo and searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <h1>Login</h1>  
         </Route>
         <Route path="/">
+        <Header/>
         <h1>Home Page</h1>  
         </Route> 
       </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import "../styles/Header.css"
+
+function Header() {
+  return (
+    <nav className="header">
+     <Link to="/">
+     <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo"/>
+     </Link> 
+     <input type="text" className="header__searchInput"/>
+    </nav>
+  )
+}
+
+export default Header
