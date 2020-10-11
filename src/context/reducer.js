1834a27e7c678fd42ba2d08bf3573ddfa323add1
Jorge <jorgediazok@gmail.com>
2020-10-11 14:54:21 -0300

    finished with checkout

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -6,13 +6,17 @@ export const initialState ={
 const reducer = (state, action) => {
   console.log(action)
   switch(action.type){
+    
     case "ADD_TO_BASKET": 
-    //Logic for adding item to basket
+        //Logic for adding item to basket
     return {...state, basket:[...state.basket, action.item]}
     break;
+    
     case "REMOVE_FROM_BASKET":
-    //Logic for remove item from basket
+        //Logic for remove item from basket
+    //Cloned the basket
     let newBasket = [...state.basket];
+    //Check if product exists
     const index = state.basket.findIndex((basketItem)=>basketItem.id === action.id);
     if(index >= 0){
       //item exists in basket, remove it
