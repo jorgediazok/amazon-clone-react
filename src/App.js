27a492ac6c7df500a0be776097c9f8ec8a9c0202
Jorge <jorgediazok@gmail.com>
2020-10-11 02:40:15 -0300

    added conditional to basket. End for today

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Checkout from "./components/Checkout"
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Switch>
        <Route path="/checkout">
        <Header/>
-        <h1>Checkout</h1> 
+        <Checkout />
         </Route>
         <Route path="/login">
         <h1>Login</h1>  
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -9,7 +9,17 @@ function Checkout() {
 
   return (
     <div className="checkout">
-      <h1>Checkout</h1>
+      <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
+      {basket?.length === 0 ? (
+        <div>
+          <h2>Your Shopping Basket is empty</h2>
+          <p>You have no items in your basket. To buy one please press in "Add to basket" next to the item.</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Your Shopping Basket</h2>
+        </div>
+      )}
     </div>
   )
 }
