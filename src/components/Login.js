13376f8389a2bace96fe7c594cbe919194696073
Jorge <jorgediazok@gmail.com>
2020-10-12 00:55:30 -0300

    adding functionality to the buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from "react-router-dom"
+import { useStateValue } from '../context/StateProvider';
 import "../styles/Login.css"
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
+  const signIn ()
+
   return (
     <div className="login">
       <Link to="/">
@@ -12,10 +19,10 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <button type="submit" className="login__signInButton" onClick={signIn}>Sign In</button>
         </form>
         <p>By signing-in you agree to Amazon Fake Clone Conditions of Use & Sale. 
           Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads.</p>
