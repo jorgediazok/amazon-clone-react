bbe026f0f8499b3b2d6ee5df1f255ea6381c97d9
Jorge <jorgediazok@gmail.com>
2020-10-12 02:38:33 -0300

    frontend complete

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,7 +6,7 @@ import "../styles/Checkout.css"
 
 function Checkout() {
 
-  const[{basket}, dispatch] = useStateValue();
+  const[{basket, user}, dispatch] = useStateValue();
 
 
   return (
@@ -15,11 +15,13 @@ function Checkout() {
       <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
       {basket?.length === 0 ? (
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2>Your Shopping Basket is empty</h2>
           <p>You have no items in your basket. To buy one please press in "Add to basket" next to the item.</p>
         </div>
       ) : (
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map(item=>(
             <CheckoutProduct 
