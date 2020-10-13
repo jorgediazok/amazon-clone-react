48c94b4d6a1bc2eae45a14eedbe4f79c9215bde1
Jorge <jorgediazok@gmail.com>
2020-10-12 22:24:41 -0300

    css of payment done

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import { useStateValue } from '../context/StateProvider'
 import "../styles/Payment.css"
 import CheckoutProduct from './CheckoutProduct'
@@ -11,6 +12,9 @@ function Payment() {
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
